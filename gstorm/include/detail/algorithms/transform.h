d4bbc360666a5b1f9f437546a6f8c1115ebc4554
Toomas Remmelg <toomas.remmelg@codeplay.com>
2017-12-13 16:48:10 +0000

    Allow non multiples of the local size in transform

--- gstorm/include/detail/algorithms/transform.h
+++ gstorm/include/detail/algorithms/transform.h
@@ -19,10 +19,10 @@ namespace algorithm {
 template <int x, typename... Ts>
 class TransformKernel{};
 
-
 template<typename InRng, typename OutRng, typename UnaryFunc>
 void transform(const InRng &in, OutRng &out, UnaryFunc func, cl::sycl::handler &cgh) {
-  const size_t global_thread_count = ranges::distance(in);
+  const size_t size = ranges::v3::distance(in);
+  const size_t global_thread_count = size % 128ul == 0ul ? size: (size/128ul + 1) * 128ul;
   const size_t local_thread_count = ranges::min(128ul, global_thread_count);
 
   cl::sycl::nd_range<1> config{global_thread_count, local_thread_count};
@@ -32,7 +32,8 @@ void transform(const InRng &in, OutRng &out, UnaryFunc func, cl::sycl::handler &
 
   cgh.parallel_for<class TransformKernel<1, UnaryFunc>>(config, [=](cl::sycl::nd_item<1> id) {
     auto gid = static_cast<size_t>(id.get_global(0));
-    *(outIt + gid) = func(*(inIt + gid));
+    if (gid < size)
+      *(outIt + gid) = func(*(inIt + gid));
   });
 }
 
