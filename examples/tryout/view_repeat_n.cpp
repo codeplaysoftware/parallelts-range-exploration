a49c9b04fa00613eb2a16d2e1837ebf418b20d36
Toomas Remmelg <toomas.remmelg@codeplay.com>
2017-12-18 13:27:02 +0000

    Add more views

---
+++ examples/tryout/view_repeat_n.cpp
@@ -0,0 +1,45 @@
+#include <gstorm.h>
+#include <vector>
+#include <iostream>
+#include <range/v3/all.hpp>
+
+#include "experimental.h"
+
+class TripleNum {
+  public:
+  constexpr TripleNum() {};
+  int operator()(int a) const {
+    return a*3;
+  }
+};
+
+int main() {
+
+  size_t vsize = 1024;
+
+  auto add3 = [](auto a) { return a + 3; };
+
+  std::vector<int> vb(vsize);
+
+  {
+    gstorm::sycl_exec exec;
+
+    auto gb = std::experimental::copy(exec, vb);
+
+    auto input = ranges::view::repeat_n(3, vsize)
+               | ranges::view::transform(add3);
+
+    std::experimental::transform(exec, input, gb, TripleNum{});
+  }
+
+  auto expected = ranges::view::repeat_n(3, vsize)
+                | ranges::view::transform(add3)
+                | ranges::view::transform(TripleNum{});
+
+  if (not ranges::equal(expected, vb)) {
+    std::cout << "Mismatch between expected and actual result\n";
+    return 1;
+  }
+
+  std::cout << "All good!\n";
+}
---
+++ examples/tryout/view_take_while.cpp
@@ -0,0 +1,64 @@
+#include <gstorm.h>
+#include <vector>
+#include <iostream>
+#include <memory>
+#include <range/v3/all.hpp>
+#include <type_traits>
+
+#include <CL/sycl.hpp>
+
+#include "experimental.h"
+
+using namespace gstorm;
+using namespace cl::sycl;
+using namespace ranges::v3;
+
+class TripleNum {
+  public:
+  constexpr TripleNum() {};
+  int operator()(int a) const {
+    return a*3;
+  }
+};
+
+class Add3 {
+  public:
+  constexpr Add3() {};
+  int operator()(int a) const {
+    return a+3;
+  }
+};
+
+int main() {
+
+  size_t vsize = 1024;
+
+  std::vector<int> va(vsize);
+  std::vector<int> vb(vsize);
+
+  for (size_t i = 0; i < vsize; i++) {
+    va[i] = i;
+  }
+
+  {
+    sycl_exec exec;
+
+    auto ga = std::experimental::copy(exec, va);
+    auto gb = std::experimental::copy(exec, vb);
+
+    std::experimental::transform(exec, view::transform(ga | view::take_while([](auto a) { return a < 512; }), Add3{}), gb, TripleNum{});
+  }
+
+  std::vector<int> gold(vsize);
+
+  transform(view::transform(view::ints(0, 1024) | view::take_while([](auto a) { return a < 512; }), Add3{}), gold.begin(), TripleNum{});
+
+  for (size_t i = 0; i < vsize; i++) {
+    if (gold[i] != vb[i]) {
+      std::cout << "Mismatch at position " << i << "\n";
+      return 1;
+    }
+  }
+
+  std::cout << "All good!\n";
+}
