9f6dd9a2f634d428f732f5fc4749645bb359e481
Toomas Remmelg <toomas.remmelg@codeplay.com>
2018-01-23 14:31:21 +0000

    Add full sgemv

--- examples/tests/sgemv.cpp
+++ examples/tests/sgemv.cpp
@@ -9,6 +9,7 @@
 #include <range/v3/all.hpp>
 
 #include "experimental.h"
+#include "my_zip.h"
 
 struct Sgemv : public testing::Test {};
 
@@ -20,6 +21,15 @@ struct DotProduct {
     auto x = std::get<1>(tpl);
 
     return ranges::inner_product(row, x, 0.0f);
+  }
+};
+
+struct MultiplyComponents {
+  constexpr MultiplyComponents() {};
+
+  template <typename T>
+  int operator()(const T& a) const {
+    return std::get<0>(a) * std::get<1>(a);
   }
 };
 
@@ -60,6 +70,62 @@ TEST_F(Sgemv, MatrixVectorMultplication) {
   auto expected = ranges::view::zip(A | ranges::view::chunk(vsize),
                     ranges::view::repeat_n(x, vsize))
                 | ranges::view::transform(DotProduct{});
+
+  EXPECT_TRUE(ranges::equal(expected, output));
+}
+
+TEST_F(Sgemv, sgemv) {
+  const size_t vsize = 1024;
+
+  std::default_random_engine generator;
+  std::uniform_real_distribution<float> distribution(0.0, 10.0);
+
+  auto generate_float = [&generator, &distribution]() {
+    return distribution(generator);
+  };
+
+  // Input to the SYCL device
+  std::vector<float> A(vsize * vsize);
+  std::vector<float> x(vsize);
+  std::vector<float> y(vsize);
+
+  ranges::generate(A, generate_float);
+  ranges::generate(x, generate_float);
+  ranges::generate(y, generate_float);
+
+  const auto a = generate_float();
+  const auto b = generate_float();
+
+  std::vector<float> output(vsize);
+
+  {
+    gstorm::sycl_exec exec;
+
+    auto gpu_A = std::experimental::copy(exec, A);
+    auto gpu_x = std::experimental::copy(exec, x);
+    auto gpu_y = std::experimental::copy(exec, y);
+    auto gpu_output = std::experimental::copy(exec, output);
+
+    auto a_rows = gpu_A | ranges::view::chunk(vsize);
+    auto x_repeat = ranges::view::repeat_n(gstorm::gpu::ref(gpu_x), vsize);
+
+    auto dot_prod = ranges::view::zip(a_rows, x_repeat)
+                  | ranges::view::transform(DotProduct{})
+                  | ranges::view::transform([a](auto dot) { return dot * a; });
+
+    auto zipped = my_zip(dot_prod, gpu_y | ranges::view::transform([b](auto y) {return b*y; }));
+
+    std::experimental::transform(exec, zipped, gpu_output, MultiplyComponents{});
+  }
+
+  auto expected = ranges::view::zip(
+                    ranges::view::zip(
+                      A | ranges::view::chunk(vsize),
+                      ranges::view::repeat_n(x, vsize))
+                    | ranges::view::transform(DotProduct{})
+                    | ranges::view::transform([a](auto dot) { return dot * a; }),
+                    y | ranges::view::transform([b](auto y) {return b*y; }))
+                | ranges::view::transform(MultiplyComponents{});
 
   EXPECT_TRUE(ranges::equal(expected, output));
 }
